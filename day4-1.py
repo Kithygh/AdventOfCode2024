f0a4efceef21224e2de03b64b6de8c841926c048
Kit Hygh <kithygh@gmail.com>
2024-12-05 13:16:18 -0500

    Da 4-1 complete.

--- day4-1.py
+++ day4-1.py
@@ -6,69 +6,56 @@ from icecream import ic
 input = Path("day4input.txt")
 # input = Path("day4inputTEST.txt")
 
-def count_xmas(chars_4) -> int:
+def count_xmas(windows) -> int:
     # ic(chars_4)
     count = 0
-    for blah in chars_4:
-        for y in blah:
-            test = ''.join(y)
+    for blah in windows:
+        for row in blah:
+            test = ''.join(row)
             # print(f"{test=}")
             if test == "XMAS" or test == "SAMX":
                 count += 1
     return count
 
-def create_diagrl(data: np.ndarray) -> np.ndarray:
-    diag_rl: list[list[int]] = [data.diagonal(i).tolist() for i in range(-data.shape[0]+1, data.shape[1])]
-    biggest = max([len(d) for d in diag_rl])
-    for x in diag_rl:
-        x.extend(['.']*(biggest-len(x)))
-    return np.array(diag_rl)
+def create_diag_view(data:np.ndarray) -> np.ndarray:
+    diags: list[list[int]] = [data.diagonal(i).tolist() for i in range(-data.shape[0]+1, data.shape[1])]
+    longest = max([len(d) for d in diags])
+    for x in diags:
+        x.extend(['.']*(longest-len(x)))
+    return np.array(diags)   
 
-def create_diaglr(data: np.ndarray) -> np.ndarray:
-    diag_rl: list[list[int]] = [np.fliplr(data).diagonal(i).tolist() for i in range(-data.shape[0]+1, data.shape[1])]
-    biggest = max([len(d) for d in diag_rl])
-    for x in diag_rl:
-        x.extend(['.']*(biggest-len(x)))
-    return np.array(diag_rl)
-    
-
-def create_views(data: np.ndarray):
-    total = 0
-    
+def create_window_views(data: np.ndarray):
     # horizontal - do nothing
-    horiz = np.lib.stride_tricks.sliding_window_view(data, 4, 1)
-    # ic(data)
-    total += count_xmas(horiz)
+    yield np.lib.stride_tricks.sliding_window_view(data, 4, 1)
     
-    # vertical
-    vert = np.lib.stride_tricks.sliding_window_view(data, 4, 0)
-    total += count_xmas(vert)
+    # vertical - use different axis for sliding window
+    yield np.lib.stride_tricks.sliding_window_view(data, 4, 0)
 
     # diagonal lr
-    # diag_rl = [data.diagonal(i).tolist() for i in range(-data.shape[0]+1, data.shape[1])]
-    diag_rl = create_diagrl(data)
-    # ic(diag_rl)
-    total += count_xmas(np.lib.stride_tricks.sliding_window_view(diag_rl, 4, 1))
+    diag_rl = create_diag_view(data)
+    yield np.lib.stride_tricks.sliding_window_view(diag_rl, 4, 1)
     
     # diagonal rl
-    diag_lr = create_diaglr(data)
-    total += count_xmas(np.lib.stride_tricks.sliding_window_view(diag_lr, 4, 1))
+    diag_lr = create_diag_view(np.fliplr(data))
+    yield np.lib.stride_tricks.sliding_window_view(diag_lr, 4, 1)
     
-    return total
 
-def create_array():
+def get_lines():
     with input.open() as f:
-        lines = f.readlines()
+        return f.readlines()
 
+def create_array(raw_data: list[str]):
     cleaned=[]    
-    for line in lines:
+    for line in raw_data:
         cleaned.append(list(line.strip()))
-        
     return np.array(cleaned)
         
 def main():
-    data = create_array()
-    total = create_views(data)
+    raw_data = get_lines()
+    data = create_array(raw_data)
+    total = 0
+    for view in create_window_views(data):
+        total += count_xmas(view)
     print(f"{total=}")
 
 
